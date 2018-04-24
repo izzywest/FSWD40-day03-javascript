3657f466a6f21fb78cb1add38795410345f94aad
schahnaz <schahnaz@mail.de>
2018-04-24 10:30:16 +0200

    add exercise 1 and 2

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-{\rtf1}
+function myFunction1() {
+    var randnb = Math.floor((Math.random() * 25) - 5);
+
+    document.getElementById("randomnb").innerHTML = randnb;
+
+    if (randnb <= 10) {
+    	var msg = "The weather is cold";
+    	document.getElementById("message").innerHTML = msg;
+
+    } else if (randnb >= 11) {
+    	var msg = "The weather is moderate";
+    	document.getElementById("message").innerHTML = msg;
+    }
+}
+
+function myFunction2() {
+	var numberHigh = Math.max(1, 7, 5, 9);
+    document.getElementById("highest").innerHTML = numberHigh;
+}
